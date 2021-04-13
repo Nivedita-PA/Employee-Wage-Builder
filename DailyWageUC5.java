23780036c7c5a26ec301a15ed912de9f6168405f
Nivedita-PA <npal4661@gmail.com>
2021-04-14 01:18:19 +0530

    [Nivedita]: added the monthly wage calculation

---
+++ DailyWageUC5.java
@@ -0,0 +1,14 @@
+public class DailyWageUC5 {
+
+	public static void main(String[] args) {
+		int wage_Per_Hr = 20;
+		int working_Hrs_Day = 8;
+		int days_Month = 20;
+		int daily_Wage = wage_Per_Hr * working_Hrs_Day * days_Month;
+		System.out.println("Wage of each employee per month");
+		System.out.println(daily_Wage);
+	
+
+	}
+
+}
